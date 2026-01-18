a22973edf50c61ecdd07c4b6e26460330d1d1de2
yunchol <yuncholdesu@gmail.com>
2026-01-18 16:38:39 +0900

    step4

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import socket
+import select
 
 HOST = "127.0.0.1"
 PORT = 8080
@@ -6,30 +7,46 @@ PORT = 8080
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server_socket.bind((HOST, PORT))
-server_socket.listen(1)
+server_socket.listen(10)
+server_socket.setblocking(False)
+
+sockets = [server_socket]
 
 print(f"Listening on http://{HOST}:{PORT}")
 
-client_socket, client_address = server_socket.accept()
-print("Connected from", client_address)
+while True:
+    readable, _, _ = select.select(sockets, [], [])
 
-request = client_socket.recv(4096)
-print("====== REQUEST ======")
-print(request.decode(errors="replace"))
-print("=====================")
+    for sock in readable:
+        # 新しい接続が来た
+        if sock is server_socket:
+            client_socket, addr = server_socket.accept()
+            client_socket.setblocking(False)
+            sockets.append(client_socket)
+            print("New connection:", addr)
 
-# ★ STEP2の主役
-body = "Hello, webserv!"
-body_bytes = body.encode()
+        # 既存クライアントからデータ
+        else:
+            data = sock.recv(4096)
 
-response = (
-    "HTTP/1.1 200 OK\r\n"
-    "Content-Type: text/plain\r\n"
-    f"Content-Length: {len(body_bytes)}\r\n"
-    "\r\n"
-).encode() + body_bytes
+            if not data:
+                print("Disconnected")
+                sockets.remove(sock)
+                sock.close()
+                continue
 
-client_socket.sendall(response)
+            print("====== REQUEST ======")
+            print(data.decode(errors="replace"))
+            print("=====================")
 
-client_socket.close()
-server_socket.close()
+            body = "Hello from event loop!"
+            body_bytes = body.encode()
+
+            response = (
+                "HTTP/1.1 200 OK\r\n"
+                "Content-Type: text/plain\r\n"
+                f"Content-Length: {len(body_bytes)}\r\n"
+                "\r\n"
+            ).encode() + body_bytes
+
+            sock.sendall(response)
